4834bd902f8a03db4eac51637b2ad078f8e66b5a
Facet Framer <facet@facetframer.com>
2017-01-04 15:54:33 +0000

    Get test.sh working cask

--- test.sh
+++ test.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+# It seems like testing something as interactive as helm is difficult
+#   so I'm not even going to try.
+
+# However, I will *lint* the file, and make sure installation works
+
 set -o errexit
 set -o nounset
 set -o pipefail
@@ -11,24 +16,8 @@ cask install
 cask build
 cask package
 
-elpa="$(mktemp -d)"
-
-# It seems like testing something as interactive as helm is difficult
-#   so I'm not even going to try.
-
-# However, I will *lint* the file, and make sure installation works
-
-package_file=$(find dist -name "*.tar")
-
-emacs -q --batch --eval "
+cask eval "
 (progn
-(setq package-user-dir \"$elpa\")
-(require 'package)
-(add-to-list 'package-archives '(\"melpa-stable\" . \"http://stable.melpa.org/packages/\") t)
-(package-initialize)
-(package-refresh-contents)
-(package-install-file \"$package_file\")
-
 (require 'orgnav)
 (require 'orgnav-tree)
 (require 'orgnav-capture)
